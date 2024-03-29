a99d1b5990f460657d0680b4cfd53b53b28a5231
KimWash <ckm0728wash@gmail.com>
2024-03-29 23:44:23 +0900

    FEAT: 코드 하이라이터 복사 기능 추가
    - 친구가 만들어달래서..

---
+++ components/CodePreview.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import "katex/dist/katex.min.css"; // `rehype-katex` does not import the CSS for you
+import {
+  Prism as SyntaxHighlighter,
+  SyntaxHighlighterProps,
+} from "react-syntax-highlighter";
+
+export default function CodePreview(props: SyntaxHighlighterProps) {
+  return (
+    <div className="relative">
+      {props.showLineNumbers && (
+        <button
+        onClick={() => {
+          navigator.clipboard.writeText(String(props.children))
+          alert("클립보드에 복사되었습니다!")
+        }}
+         className="absolute bottom-0 right-0 bg-white rounded-lg m-2 p-1 text-sm">
+          Copy
+        </button>
+      )}
+      <SyntaxHighlighter {...props}>{props.children}</SyntaxHighlighter>
+    </div>
+  );
+}
--- components/MarkdownRenderer.tsx
+++ components/MarkdownRenderer.tsx
@@ -2,9 +2,8 @@ import ReactMarkdown from "react-markdown";
 import gfm from "remark-gfm";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
-import "katex/dist/katex.min.css"; // `rehype-katex` does not import the CSS for you
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { darcula } from "react-syntax-highlighter/dist/esm/styles/prism";
+import CodePreview from "./CodePreview";
 
 export default function MarkdownRenderer({ content }: { content: string }) {
   return (
@@ -15,14 +14,26 @@ export default function MarkdownRenderer({ content }: { content: string }) {
         code(props) {
           const { children, className, node, ...rest } = props;
           const match = /language-(\w+)/.exec(className || "");
-          return match ? (
-            <SyntaxHighlighter PreTag="div" language={match[1]} style={darcula} showLineNumbers customStyle={{borderRadius: 14}} >
+          const isCodeBlock = match;
+          return isCodeBlock ? (
+            <CodePreview
+              PreTag="div"
+              language={match[1]}
+              style={darcula}
+              showLineNumbers
+              customStyle={{ borderRadius: 14 }}
+              
+            >
               {String(children).replace(/\n$/, "")}
-            </SyntaxHighlighter>
+            </CodePreview>
           ) : (
-            <SyntaxHighlighter PreTag="span" style={darcula} customStyle={{padding:0}} >
+            <CodePreview
+              PreTag="span"
+              style={darcula}
+              customStyle={{ padding: 0 }}
+            >
               {String(children)}
-            </SyntaxHighlighter>
+            </CodePreview>
           );
         },
       }}
