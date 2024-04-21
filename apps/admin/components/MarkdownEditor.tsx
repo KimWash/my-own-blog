b1cd78561e2fc7dce056724aed462e4aa7a95621
KimWash <ckm0728wash@gmail.com>
2024-04-21 15:39:28 +0900

    FEAT: Toast UI Editor로 전환
    - latex 커스텀 렌더러 적용

--- apps/admin/components/MarkdownEditor.tsx
+++ apps/admin/components/MarkdownEditor.tsx
@@ -1,22 +1,66 @@
 "use client";
 
+import "@toast-ui/editor/dist/toastui-editor.css";
+import "prismjs/themes/prism.css";
+import "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css";
+
+// import { Editor } from "@toast-ui/react-editor";
+import { ForwardedRef, forwardRef } from "react";
 import dynamic from "next/dynamic";
-import { forwardRef } from "react";
-import { type MDXEditorMethods, type MDXEditorProps } from "@mdxeditor/editor";
-// ForwardRefEditor.tsx
-
-// This is the only place InitializedMDXEditor is imported directly.
-const Editor = dynamic(() => import("./InitializeMDXEditor"), {
-  // Make sure we turn SSR off
-  ssr: false,
-});
-
-// This is what is imported by other components. Pre-initialized with plugins, and ready
-// to accept other props, including a ref.
-const MarkdownEditor = forwardRef<MDXEditorMethods, MDXEditorProps>(
-  (props, ref) => <Editor {...props} editorRef={ref} />
+const Editor = dynamic(
+  () => import("@toast-ui/react-editor").then((module) => module.Editor),
+  { ssr: false }
 );
-export default MarkdownEditor;
+const codeSyntaxHighlightPlugin = dynamic(
+  () => import("@toast-ui/editor-plugin-code-syntax-highlight")
+);
+import Prism from "prismjs";
+import { HtmlGenerator, parse, TuiNode } from "latex.js";
 
-// TS complains without the following line
-MarkdownEditor.displayName = "MarkdownEditor";
+export default forwardRef(function ToastMarkdownEditor(
+  {
+    initialMarkdown,
+  }: {
+    initialMarkdown?: string;
+  },
+  ref: ForwardedRef<typeof Editor>
+) {
+  return (
+    <Editor
+      height="auto"
+      ref={ref}
+      initialValue={initialMarkdown ?? ""}
+      previewStyle="vertical"
+      initialEditType="markdown"
+      plugins={[[codeSyntaxHighlightPlugin, { highlighter: Prism }]]}
+      hideModeSwitch
+      customHTMLRenderer={{
+        latex(node: TuiNode) {
+          const generator = new HtmlGenerator({ hyphenate: false });
+          const { body } = parse(node.literal, { generator }).htmlDocument();
+
+          // HTML 파싱해서 렌더된 수식만을 남기고 제거
+          const domParser = new DOMParser();
+          const dom = domParser.parseFromString(body.innerHTML, "text/html");
+          dom.querySelector(".katex-html")?.remove();
+
+          return [
+            { type: "openTag", tagName: "div", outerNewLine: true },
+            { type: "html", content: dom.querySelector("body")?.innerHTML },
+            { type: "closeTag", tagName: "div", outerNewLine: true },
+          ];
+        },
+      }}
+      hooks={{
+        addImageBlobHook(
+          blob: File,
+          callback: (imageUrl: string, altText: string) => void
+        ) {
+          // Todo: Implement Upload
+          console.log(blob);
+          callback("/api/media/1/HIGH", "test");
+        },
+      }}
+    />
+  );
+});
--- apps/admin/components/container/PostCreateContainer.tsx
+++ apps/admin/components/container/PostCreateContainer.tsx
@@ -2,29 +2,30 @@
 
 import MarkdownEditor from "@/components/MarkdownEditor";
 import { useRef, useState } from "react";
-import { type MDXEditorMethods } from "@mdxeditor/editor";
 
 export default function PostCreateContainer({
   initialMarkdown,
 }: {
   initialMarkdown?: string;
 }) {
-  const editorRef = useRef<MDXEditorMethods>(null);
-  const [markdown, setMarkdown] = useState(initialMarkdown ?? '');
+  const [markdown, setMarkdown] = useState(initialMarkdown ?? "");
   return (
-  <>
-  <button onClick={() => console.log(markdown)}></button>
-  {  <MarkdownEditor
-      ref={editorRef}
-      markdown={initialMarkdown ?? ''}
-      onChange={setMarkdown}
-      // onDrop={(e) => {
-      //   e.preventDefault();
-      //   const file = e.dataTransfer.files?.[0];
-      //   if (file) {
-
-      //   }
-      // }}
-    />}</>
+    <>
+      <button onClick={() => console.log(markdown)}></button>
+      {
+        // <MarkdownEditor
+        //     ref={editorRef}
+        //     markdown={initialMarkdown ?? ''}
+        //     onChange={setMarkdown}
+        // onDrop={(e) => {
+        //   e.preventDefault();
+        //   const file = e.dataTransfer.files?.[0];
+        //   if (file) {
+        //   }
+        // }}
+        // />
+        <MarkdownEditor  initialMarkdown={markdown} />
+      }
+    </>
   );
 }
