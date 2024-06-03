a1e76eda87317da5ca9e7c54ea9c9bb940143f89
KimWash <ckm0728wash@gmail.com>
2024-06-03 20:08:49 +0900

    FEAT: 카테고리 필드 추가 및 어드민측 필터링 구현

--- packages/app/admin/app/post/[...categories]/page.tsx
+++ packages/app/admin/app/post/[...categories]/page.tsx
@@ -1,5 +1,5 @@
 import Tag from "@my-own-blog/core/components/Tag";
-import client, { Post } from "@my-own-blog/db";
+import client, { Category, Post } from "@my-own-blog/db";
 import "@my-own-blog/core/lib/date/date.extensions";
 import Table, { Column } from "@/components/Table";
 import Link from "next/link";
@@ -9,11 +9,29 @@ export default async function Page({
 }: {
   params: { categories: string[] };
 }) {
+  const currentCategory = await client.category.findUnique({
+    where: {
+      id: params.categories[params.categories.length - 1],
+    },
+    // Todo: 카테고리 트리 구조를 `앱 > Flutter`와 같이 표시하려 함.
+    // 이 상태로는 2 depth menu만을 지원. 메뉴 레벨에 따라 재귀적으로 include 옵션을 생성하게 하는 로직이 필요하려나?
+    // 아니면 Prisma가 이런 기능을 지원하려나?
+    include: {
+      parent: true,
+    },
+  });
+  if (!currentCategory) return <div>올바르지 않은 카테고리!</div>;
   const posts = await client.post.findMany({
     where: {
       is_deleted: undefined,
+      category: {
+        id: {
+          equals: currentCategory?.id,
+        },
+      },
     },
   });
+
   const columns = [
     {
       name: "title",
@@ -44,9 +62,12 @@ export default async function Page({
       },
     },
   ] as Column<Post>[];
+
   return (
-    <main>
-      <h3>URL: /post/{params.categories.join("/")}</h3>
+    <main className="p-4">
+      <h1 className="text-2xl font-bold">
+        {`${currentCategory.parent?.name} > ${currentCategory.name}`}
+      </h1>
       <Link href="/post/create">
         <button className="btn">글 작성</button>
       </Link>
