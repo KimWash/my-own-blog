d6cdf7ae15eb3ae154670fb396e3001f142b4056
KimWash <ckm0728wash@gmail.com>
2024-06-29 22:49:32 +0900

    FIX: edit tsconfig rootDir

--- packages/lib/core/service/PostService.ts
+++ packages/lib/core/service/PostService.ts
@@ -2,18 +2,19 @@ import { PostDetailDto } from "../lib/model/Post";
 import db from "@my-own-blog/db";
 
 export class PostService {
-  static async getPost(id: number): Promise<PostDetailDto>{
+  static async getPost(id: number): Promise<PostDetailDto> {
     const post = await db.post.findFirst({
       where: { id: Number(id), is_deleted: undefined },
       include: {
         tags: { include: { tag: true } },
         medias: { include: { files: true } },
+        category: true,
       },
     });
-    if (!post) throw new Error('Post not found')
+    if (!post) throw new Error("Post not found");
     const medias = post?.medias ?? [];
     const tags = post?.tags.map((postTag) => postTag.tag) ?? [];
-    
-    return {...post, medias, tags };
+
+    return { ...post, category: post.category!, medias, tags };
   }
 }
