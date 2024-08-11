93ca1171f419f9f0dee2cd141b3f5584b2c02026
KimWash <ckm0728wash@gmail.com>
2024-08-11 14:07:12 +0900

    FIX: session check

--- packages/app/admin/app/page.tsx
+++ packages/app/admin/app/page.tsx
@@ -4,6 +4,6 @@ import { redirect } from "next/navigation";
 export default async function Home() {
   const session = await auth();
 
-  if (session) redirect("/dashboard");
+  if (session?.user) redirect("/dashboard");
   else redirect("/signin");
 }
