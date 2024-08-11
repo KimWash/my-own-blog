82be5b97705fe564ea967131003620cbd8217725
KimWash <ckm0728wash@gmail.com>
2024-08-11 23:32:43 +0900

    FEAT: Implement meta

--- packages/app/client/app/magazine/post/[id]/page.tsx
+++ packages/app/client/app/magazine/post/[id]/page.tsx
@@ -3,6 +3,31 @@ import { PostDetailQueryKey } from "@/components/queries/usePostQuery";
 import getDehydratedState from '@my-own-blog/core/lib/query/useDehydratedState';
 import { PostService } from "@/components/model/PostService";
 import { Hydration } from "@my-own-blog/core/lib/query/Hydration";
+import { Metadata, ResolvingMetadata } from "next";
+
+type Props = {
+  params: { id: number }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+ 
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const id = params.id
+  const post = await PostService.fetchPost(id);
+ 
+  return {
+    title: post.title,
+    openGraph: {
+      images: [`/media/${post.medias.find(media => media.id === post.thumbnail_media)?.id}/HIGH`],
+      description: post.description!,
+      releaseDate: post.create_dt?.toString()
+    },
+  }
+}
+
 
 export default async function Page({ params }: { params: { id: number } }) {
   const dehydratedState = await getDehydratedState({
