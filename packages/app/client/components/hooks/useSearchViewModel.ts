4fe21510d203a69ec030898d68607c3acccd7fe0
KimWash <ckm0728wash@gmail.com>
2024-08-11 13:35:52 +0900

    FIX: blogtype

--- packages/app/client/components/hooks/useSearchViewModel.ts
+++ packages/app/client/components/hooks/useSearchViewModel.ts
@@ -1,11 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
-import { PostQueryKey, PostSearchParam } from "../queries/usePostListQuery";
+import { BlogType, PostQueryKey, PostSearchParam } from "../queries/usePostListQuery";
 import { PostService } from "../model/PostService";
 
 export default function useSearchViewModel(param: PostSearchParam) {
   return useQuery({
     queryKey: PostQueryKey.search(param),
     queryFn: ({ queryKey: [_, page, query] }) =>
-      PostService.fetchPosts(Number(page), query?.toString()),
+      PostService.fetchPosts(Number(page), query as BlogType),
   });
 }
